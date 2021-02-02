2d9a1272b3bda5a3567839d09908420797bebdcf
hengchengkiat@gmail.com <04071997>
2021-02-02 12:03:05 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Buttons from '@material-ui/core/Button';
+import './App.css';
+
+
+function App (){
+return(
+    <div>
+        <div>hello world</div>
+        <div className='container'>
+            <button class='btn'>1</button>
+            <button class='btn1'>2</button>
+            
+            
+        </div>
+
+
+    </div>
+
+
+)
+}
+
+
+
+export default App;
+
