f57b4d39afa69faf23e63565e7adf93991210597
casslzh <cass.lzh@gmail.com>
2021-03-06 12:38:09 +0800

    update

--- src/login/Login.js
+++ src/login/Login.js
@@ -1,103 +1,109 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import { FormGroup, FormControl, FormLabel } from "react-bootstrap";
-import { useHistory } from "react-router-dom";
-//import { useDispatch } from "react-redux";
-import "./Login.css";
-import { authenticateLogin } from '../redux/ducks/auth';
+import React, { Component } from "react";
+import { Button, CssBaseline, TextField, Container } from "@material-ui/core";
+import { authenticateLogin } from "../redux/ducks/auth";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import ResultController from '../results/ResultController.jsx';
+import {Route} from "react-router-dom";
 
-export default function Login() {
+//import "./Login.css";
 
-
+ 
+class LoginPage extends Component {
+  state = {
+    username: "",
+    password: "",
+   };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
   
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const history = useHistory();
-  //const dispatch = useDispatch();
+    
+  handleSubmit = (e) => {
 
-
-  function validateForm() {
-    return email.length > 0 && password.length > 0;
-  }
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-     // this.setState({submitted:true});
     try {
-      await (authenticateLogin({username: email, password: password}));
-      history.push("/App");
+      this.props.authenticateLogin(this.state);
+      this.props.history.push('/results');
     } catch (e) {
       alert(e.message);
     }
-  }
- 
-//   function handleSubmit(username,password) {
-//     return dispatch => {
-//       //dispatch(({ username }));
+    
+   };
 
-//       authenticateLogin.login(username, password)
-//           .then(
-//               user => { 
-//                   dispatch(authenticateLogin({username: email, password: password}));
-//                   history.push('/App');
-//               },
-//               error => {
-//                 alert();
-//               }
-//           );
-//   };
- 
-// }
-
-//     return dispatch =>{
-//      event.preventDefault();
-//     try {
-//       dispatch(authenticateLogin({username: email, password: password}));
-//       history.push("/App");
-//     } catch (e) {
-//       alert(e.message);
-//     }
-//   }
-// }
+render() {
+  const { username, password } = this.state;
+  
+  <Route key="ResultController" path="/results" exact component={ResultController} />
 
   return (
-    <div className="main">
-      <div className="login-form">  
+    <div className="main" data-test="loginContainer">
+      <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="login-form">
         <h3>Login</h3>
-        <form onSubmit={handleSubmit}>
-          <FormGroup controlId="email" bsSize="large">
-            <FormLabel>Email   </FormLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              placeholder="Enter Email"
-              value={email}
-              style={{height:25, width:200, marginLeft:55}}
-              onChange={(e) => setEmail(e.target.value)}
-            /> <br/> <br/>
-          </FormGroup> 
-          <FormGroup controlId="password" bsSize="large">
-            <FormLabel>Password     </FormLabel>
-            <FormControl
-              type="password"
-              placeholder="Enter password"
-              value={password}
-              style={{height:25, width:200, marginLeft:22}}
-              onChange={(e) => setPassword(e.target.value)}
-            /><br/> <br/>
-          </FormGroup>
+        <form
+          noValidate
+          onSubmit={this.handleSubmit}
+        >
+          <TextField
+            data-testid="usernameField"
+            onChange={this.handleChange}
+            defaultValue={username}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoComplete="username"
+            autoFocus
+            //style={{height:25, width:200, marginLeft:55}}
+          />
+          <TextField
+            data-testid="passwordField"
+            onChange={this.handleChange}
+            defaultValue={password}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            //style={{height:25, width:200, marginLeft:55}}
+          />
           <Button
+            data-testid="loginButton"
+            type="submit"
+            fullWidth
             variant="contained"
             color="secondary"
-            disabled={!validateForm()}
-            type="submit"
-            style={{marginLeft:120, marginTop:10}}
-          >
-            Login
-          </Button>
-         
+            style={{  marginTop: 20 }}
+          >Login</Button>
+        
         </form>
       </div>
+      </Container>
     </div>
-  );
+    );
+  }
+
 }
+LoginPage.propTypes = {
+  /** An action creator for authenticating login */
+  authenticateLogin: PropTypes.func.isRequired
+  /** An object used for styling */
+};
+
+const dispatchers = {
+  authenticateLogin
+};
+
+
+
+export default connect(() => ({}), dispatchers)(LoginPage);
