cdc2d92872d4a78a2ba30ba63e8bf7dbf6827a2f
casslzh <cass.lzh@gmail.com>
2021-03-17 17:17:21 +0800

    Update SystemAdmin.js, Able to get data from redux but haven't render into table on UI.

--- src/systemadmin/SystemAdmin.js
+++ src/systemadmin/SystemAdmin.js
@@ -1,209 +1,114 @@
 import React, { Component } from "react";
-import { Button, CssBaseline } from "@material-ui/core";
-import { DataGrid } from '@material-ui/data-grid';
-import { deleteUser, listUsers } from "../redux/ducks/users";
-import Container from '@material-ui/core/Container';
-import Link from '@material-ui/core/Link';
-import AppBar from '@material-ui/core/AppBar';
-import Card from '@material-ui/core/Card'
-import CardActions from '@material-ui/core/CardActions'
-import CardContent from "@material-ui/core/CardContent";
-import Grid from '@material-ui/core/Grid';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import MaterialTable from 'material-table';
+import { deleteUser, listUsers, createUser, updateUser } from "../redux/ducks/users";
+import {
+  selectUserLoading,
+  selectUserFailed,
+  selectUser  
+} from "../redux/ducks/auth";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { makeStyles } from '@material-ui/core/styles';
-
-
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        QWERTY
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
+import Loader from 'react-loader-spinner';
 
 class SystemAdmin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      tableData: [{
-        id:'',
-        email: '',
-        pass: '',
-        role: '',
-        name: '',
-        dob: '',
-        gender: '',
-      }],
-    };
-  }
-
+    
   componentDidMount() {
-    this.props.listUsers(this.state).then(response => {
-      this.setState({ tableData: response.data });
-    });
-  }
-
-  handleDelete = () => {
-
-    this.props.deleteUser(this.state)
-  }
-
-  goPage = (page) => {
-    this.props.history.push(page);
+      this.props.listUsers();
   }
 
   render() {
-      const { tableData } = this.state;
+      const {
+          usersLoading,
+          users,
+          createUser,
+          updateUser,
+          deleteUser
+      } = this.props;
 
-    // const { data } = listUsers({
-    //   dataSet: 'Users',
-    //   rowLength: 100,
-    //   maxColumns: 6,
-    // });
+      if (usersLoading)
+          return <Loader />;
 
-    const columns = [
-      { field: "_id", headerName: 'id', hide: true },
-      { field: '{this.state.tableData.email}', headerName: 'Email', width: 130 },
-      { field: '{this.state.tableData.pass}', headerName: 'Pass', width: 130 },
-      { field: '{this.state.tableData.name}', headerName: 'Name', width: 130 },
-      { field: '{this.state.tableData.dob}', headerName: 'Date of Birth', width: 150, },
-      { field: '{this.state.tableData.gender}', headerName: 'Gender', width: 130 },
-      { field: '{this.state.tableData.role}', headerName: 'Role', width: 130 },
-    ];
-    
-     const classes = makeStyles((theme) => ({
-      icon: {
-        marginRight: theme.spacing(2),
-      },
-      heroContent: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(8, 0, 6),
-      },
-      heroButtons: {
-        marginTop: theme.spacing(4),
-      },
-      cardGrid: {
-        paddingTop: theme.spacing(8),
-        paddingBottom: theme.spacing(8),
-      },
-      card: {
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-      },
-      cardMedia: {
-        paddingTop: '56.25%', // 16:9
-      },
-      cardContent: {
-        flexGrow: 1,
-      },
-      footer: {
-        backgroundColor: theme.palette.background.paper,
-        padding: theme.spacing(10),
-        marginTop: 20
-      },
-    }));
-    const cards = [{ name: 'Manage Accounts', button: "Update" }]
-
-    return (
-      <React.Fragment>
-        <CssBaseline />
-        <AppBar position="relative">
-          <Toolbar>
-            <Typography variant="h6" color="inherit" align="center" noWrap>
-              TRAIL MAKING TEST
-              </Typography>
-          </Toolbar>
-        </AppBar>
-        <main>
-          {/* Hero unit */}
-          <CssBaseline />
-          <div style={{ height: 400, width: '100%' }}>
-            <DataGrid   rowKey="id" rows={tableData} columns={columns} pageSize={5} checkboxSelection />
+      return (
+          <div className="container" data-test="adminTable">
+              <MaterialTable 
+                  title="User"
+                  columns={[
+                      {
+                          title: 'Name',
+                          field: 'name',
+                      },
+                      {
+                          title: 'Email',
+                          field: 'email',
+                      },
+                      {
+                          title: 'Role',
+                          field: 'role',
+                          lookup: {
+                              ROLE_DOCTOR: "ROLE_DOCTOR",
+                              ROLE_PATIENT: "ROLE_PATIENT",
+                              ROLE_ADMIN:"ROLE_ADMIN",
+                          },
+                      },
+                      {
+                          title: 'Password',
+                          field: 'pass',
+                      }
+                  ]}
+                  data={users}
+                  options={{}}
+                  editable={{
+                      onRowAdd: newData =>
+                          new Promise((resolve, reject) => {
+                              createUser(newData)
+                                  .then(this.setState(users, () => resolve()))
+                          }),
+                      onRowUpdate: (newData, oldData) =>
+                          new Promise((resolve, reject) => {
+                              updateUser(newData, oldData)
+                                  .then(this.setState(users, () => resolve()))
+                          }),
+                      onRowDelete: oldData =>
+                          new Promise((resolve, reject) => {
+                              deleteUser(oldData)
+                                  .then(this.setState(users, () => resolve()))
+                          }),
+                  }}
+              />
           </div>
-
-          <div style={{ marginTop: 30 }}>
-            <Container className={classes.cardGrid} maxWidth="md">
-              <Grid container direction="row" justify="center" alignItems="center" spacing={1}>
-                {cards.map((card) => (
-                  <Grid item key={card} xs={23} sm={6} md={10}>
-                    <Card className={classes.card} style={{ height: 140 }}>
-                      <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          {card.name}
-                        </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button
-                          type="link"
-                          data-testid="createuserButton"
-                          color="secondary"
-                          variant="contained"
-                          style={{ marginLeft: 70 }}
-                          onClick={() => this.goPage("/newaccount")}>
-                          Create New User
-            </Button>
-
-                        <Button
-                          type="link"
-                          data-testid="updateuserButton"
-                          color="secondary"
-                          variant="contained"
-                          style={{ marginLeft: 60 }}
-                          onClick={() => this.goPage(`/updateuser/${this.state.id}`)}>
-                          Update User
-              </Button>
-
-                        <Button
-                          type="delete"
-                          data-testid="deleteuserButton"
-                          color="secondary"
-                          variant="contained"
-                          style={{ marginLeft: 60 }}
-                          onClick={() => this.handleDelete()}>
-                          Delete User
-              </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
-          </div>
-        </main>
-        {/* Footer */}
-        <div style={{ marginTop: 30 }}>
-          <footer className={classes.footer}>
-            <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-              This is a school project for CZ3002.
-            </Typography>
-            <Copyright />
-          </footer>
-        </div>
-        {/* End footer */}
-      </React.Fragment>
-    );
+      )
   }
 }
 
 SystemAdmin.propTypes = {
-  /** An action creator for authenticating login */
+  /** A boolean to determine if the users are still being loaded by the `listUsers` action creator (true: still loading, false: fully loaded) */
+  usersLoading: PropTypes.bool.isRequired,
+  /** A boolean to determine if the users failed to be loaded by the `listUsers` action creator (true: still loading or failed to load, false: successful load) */
+  usersFailed: PropTypes.bool,
+  /** An array of users objects loaded by the `listUsers` action creator */
+  users: PropTypes.array.isRequired,
+  /** An action creator for loading accounts from the server*/
   listUsers: PropTypes.func.isRequired,
-  deleteUser: PropTypes.func.isRequired
-
+  /** An action creator for creating a user account */
+  createUser: PropTypes.func.isRequired,
+  /** An action creator for deleting a user account */
+  deleteUser: PropTypes.func.isRequired,
+  /** An action creator for updating a user account*/
+  updateUser: PropTypes.func.isRequired,
 };
+
+const mapStateToProps = (state) => ({
+  userLoading: selectUserLoading(state),
+  userFailed: selectUserFailed(state),
+  user: selectUser(state)
+});
+
 
 const dispatchers = {
+  createUser,
+  updateUser,
+  deleteUser,
   listUsers,
-  deleteUser
 };
 
-export default connect(() => ({}), dispatchers)(SystemAdmin);
+export default connect(mapStateToProps, dispatchers)(SystemAdmin);
