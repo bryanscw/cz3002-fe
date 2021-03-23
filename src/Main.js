e0d31f19b3e622024f0fbeb8b3aaea77fe3866af
suvoonhou <voonhousu@gmail.com>
2021-03-23 13:51:36 +0800

    Modified login

--- src/Main.js
+++ src/Main.js
@@ -1,64 +1,50 @@
 import Link from '@material-ui/core/Link';
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Game from './components/game/Game';
 import SystemAdmin from './components/systemadmin/SystemAdmin.js';
 import ResultController from './components/results/ResultController.jsx';
-import { BrowserRouter as Router, Route, Switch, Link as RouterLink } from "react-router-dom";
-import { Button, CssBaseline, AppBar, Toolbar, Grid, CardContent, CardActions, Card, Container, Typography } from "@material-ui/core";
-import { withStyles } from '@material-ui/styles';
+import {
+  BrowserRouter as Router,
+  Link as RouterLink,
+  Route,
+  Switch
+} from "react-router-dom";
+import {
+  AppBar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Container,
+  CssBaseline,
+  Grid,
+  Toolbar,
+  Typography
+} from "@material-ui/core";
 import PropTypes from "prop-types";
 import {
-  fetchMe,
   selectUser,
-  selectUserLoading,
-  selectUserFailed
+  selectUserFailed,
+  selectUserLoading
 } from './redux/ducks/auth.js'
-import { connect } from "react-redux";
+import {connect, Provider} from "react-redux";
 
 import store from './redux/store.js'
 
-//../redux/ducks/diagnosis
-import { Provider } from 'react-redux'
-// import { MuiThemeProvider } from '@material-ui/core/styles';
-// import { createMuiTheme } from '@material-ui/core/styles';
-//import './header.css';
-//import resultTest from './resultTest.js';
-// import ResultController from './results/ResultController.jsx';
-
 function Copyright() {
   return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        QWERTY
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
+      <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://material-ui.com/">
+          QWERTY
+        </Link>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </Typography>
   );
 }
 
-
-
-
 class Header extends Component {
-  // componentDidMount(){
-
-  //   this.props.fetchMe(localStorage.getItem('access_token'))
-  // }
-
-  // logout = () => {
-
-  //   localStorage.removeItem("access_token");
-  //     localStorage.removeItem("refresh_token");
-  //     localStorage.removeItem("expires_in");
-  //     localStorage.removeItem("current_user");
-  //     localStorage.removeItem("time_token_acquired");
-  //     localStorage.removeItem("role");
-  //   this.props.history.push(`/login`);
-  //   //  window.location.reload(false);
-
-  // }
   render = () => {
 
     if (!this.props.userLoading && !this.props.userFailed) {
@@ -67,214 +53,232 @@ class Header extends Component {
       localStorage.setItem("role", role)
     }
 
-
-
     if (localStorage.getItem("role") === "ROLE_ADMIN") {
       return (
-        <Provider store={store}>
+          <Provider store={store}>
 
-          <React.Fragment>
-            <CssBaseline />
-            <AppBar position="relative">
-              <Toolbar>
-                <Typography variant="h6" color="inherit" align="center" noWrap>
-                  TRAIL MAKING TEST
+            <React.Fragment>
+              <CssBaseline/>
+              <AppBar position="relative">
+                <Toolbar>
+                  <Typography variant="h6" color="inherit" align="center"
+                              noWrap>
+                    TRAIL MAKING TEST
                   </Typography>
-              </Toolbar>
-            </AppBar>
-            <main>
-              {/* Hero unit */}
-              <div >
-                <Container maxWidth="sm">
-                  <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                    Trail Making Test
-                    </Typography>
-                  <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                    Trail Making Test is a simple neuropsychological test of cognitive processes, including attention, visual search and scanning, and psychomotor speed.
-                    </Typography>
-                </Container>
-              </div>
-              <Router>
-                <Container maxWidth="md">
-                  <Grid container direction="row" justify="center" alignItems="center" spacing={4}>
-
-                    <Grid item xs={12} sm={6} md={4}>
-                      <Card >
-                        <CardContent >
-                          <Typography gutterBottom variant="h5" component="h2">
-                            System Admin
-                              </Typography>
-                        </CardContent>
-                        <CardActions>
-                          <RouterLink to={`/systemadmin`}>
-                            <Button assize="small" color="primary">
-                              view
-                                </Button>
-                          </RouterLink>
-                        </CardActions>
-                      </Card>
-                    </Grid>
-
-                  </Grid>
-                  <Button
-                  data-testid="loginButton"
-                  type="submit"
-                  fullWidth                  
-                  variant="contained"
-                  color="secondary"
-                href="/logout"
-               style={{ marginLeft: 400,width:120, height:50, marginTop:30, marginBottom:30 }}
-                >Logout</Button>
-     
-                </Container>
+                </Toolbar>
+              </AppBar>
+              <main>
+                {/* Hero unit */}
                 <div>
-                  <Switch>                 
-                <Route path="/systemadmin" component={SystemAdmin} />       
-                  </Switch>
+                  <Container maxWidth="sm">
+                    <Typography component="h1" variant="h2" align="center"
+                                color="textPrimary" gutterBottom>
+                      Trail Making Test
+                    </Typography>
+                    <Typography variant="h5" align="center"
+                                color="textSecondary" paragraph>
+                      Trail Making Test is a simple neuropsychological test of
+                      cognitive processes, including attention, visual search
+                      and scanning, and psychomotor speed.
+                    </Typography>
+                  </Container>
                 </div>
-                {/* <AppRouter/> */}
-              </Router>
-            </main>
-            {/* Footer */}
-            <footer >
-              <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-                This is a school project for CZ3002.
-                </Typography>
-              <Copyright />
-              
-           
-           
-            </footer>
-            {/* End footer */}
+                <Router>
+                  <Container maxWidth="md">
+                    <Grid container direction="row" justify="center"
+                          alignItems="center" spacing={4}>
 
-          </React.Fragment>
-
-        </Provider>
-
-      )
-
-    }
-
-    else if (localStorage.getItem("role") === "ROLE_DOCTOR" || localStorage.getItem("role") === "ROLE_PATIENT") {
-
-      let isPatient = localStorage.getItem("role") === "ROLE_PATIENT";
-      return (
-        <Provider store={store}>
-
-          <React.Fragment>
-            <CssBaseline />
-            <AppBar position="relative">
-              <Toolbar>
-                <Typography variant="h6" color="inherit" align="center" noWrap>
-                  TRAIL MAKING TEST
-                        </Typography>
-              </Toolbar>
-            </AppBar>
-            <main>
-              {/* Hero unit */}
-              <div >
-                <Container maxWidth="sm">
-                  <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                    Trail Making Test
-                          </Typography>
-                  <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                    Trail Making Test is a simple neuropsychological test of cognitive processes, including attention, visual search and scanning, and psychomotor speed.
-                          </Typography>
-                </Container>
-              </div>
-              <Router>
-                <Container maxWidth="md">
-                  <Grid container direction="row" justify="center" alignItems="center" spacing={4}>
-                    {isPatient ?
                       <Grid item xs={12} sm={6} md={4}>
-                        <Card >
-                          <CardContent >
-                            <Typography gutterBottom variant="h5" component="h2">
-                              Game
-                              </Typography>
+                        <Card>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5"
+                                        component="h2">
+                              System Admin
+                            </Typography>
                           </CardContent>
                           <CardActions>
-                            <RouterLink to={`/game`}>
+                            <RouterLink to={`/systemadmin`}>
                               <Button assize="small" color="primary">
-                                Start
-                                </Button>
+                                view
+                              </Button>
                             </RouterLink>
                           </CardActions>
                         </Card>
                       </Grid>
-                      : null}
 
-                    <Grid item xs={12} sm={6} md={4}>
-                      <Card >
-                        <CardContent >
-                          <Typography gutterBottom variant="h5" component="h2">
-                            Result
-                              </Typography>
-                        </CardContent>
-                        <CardActions>
-                          <RouterLink to={`/result`}>
-                            <Button assize="small" color="primary">
-                              view
-                                </Button>
-                          </RouterLink>
-                        </CardActions>
-                      </Card>
                     </Grid>
-                  </Grid>
-                  <Button
-                  data-testid="loginButton"
-                  type="submit"
-                  fullWidth                  
-                  variant="contained"
-                  color="secondary"
-                href="/logout"
-               style={{ marginLeft: 400,width:120, height:50, marginTop:30, marginBottom:30 }}
-                >Logout</Button>
-                </Container>
-                <div>
-                  <Switch>
-                        <Route path="/game" component={Game} />
-                <Route path="/result" component={ResultController} />
-                  </Switch>
-                </div>
-                {/* <AppRouter/> */}
-              </Router>
-            </main>
-            {/* Footer */}
-            <footer 
-            >
-              <Typography variant="subtitle1" align="center" color="textSecondary"  style={{ marginTop: 90 }} component="p">
-                This is a school project for CZ3002.
-                      </Typography>
-              <Copyright />
-            </footer>
-            {/* End footer */}
+                    <Button
+                        data-testid="loginButton"
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        href="/logout"
+                        style={{
+                          marginLeft: 400,
+                          width: 120,
+                          height: 50,
+                          marginTop: 30,
+                          marginBottom: 30
+                        }}
+                    >Logout</Button>
 
-           
+                  </Container>
+                  <div>
+                    <Switch>
+                      <Route path="/systemadmin" component={SystemAdmin}/>
+                    </Switch>
+                  </div>
+                  {/* <AppRouter/> */}
+                </Router>
+              </main>
+              {/* Footer */}
+              <footer>
+                <Typography variant="subtitle1" align="center"
+                            color="textSecondary" component="p">
+                  This is a school project for CZ3002.
+                </Typography>
+                <Copyright/>
 
 
-          </React.Fragment>
+              </footer>
+              {/* End footer */}
 
-        </Provider>
+            </React.Fragment>
+
+          </Provider>
 
       )
-    }
-    else {
+
+    } else if (localStorage.getItem("role") === "ROLE_DOCTOR"
+        || localStorage.getItem("role") === "ROLE_PATIENT") {
+
+      let isPatient = localStorage.getItem("role") === "ROLE_PATIENT";
+      return (
+          <Provider store={store}>
+
+            <React.Fragment>
+              <CssBaseline/>
+              <AppBar position="relative">
+                <Toolbar>
+                  <Typography variant="h6" color="inherit" align="center"
+                              noWrap>
+                    TRAIL MAKING TEST
+                  </Typography>
+                </Toolbar>
+              </AppBar>
+              <main>
+                {/* Hero unit */}
+                <div>
+                  <Container maxWidth="sm">
+                    <Typography component="h1" variant="h2" align="center"
+                                color="textPrimary" gutterBottom>
+                      Trail Making Test
+                    </Typography>
+                    <Typography variant="h5" align="center"
+                                color="textSecondary" paragraph>
+                      Trail Making Test is a simple neuropsychological test of
+                      cognitive processes, including attention, visual search
+                      and scanning, and psychomotor speed.
+                    </Typography>
+                  </Container>
+                </div>
+                <Router>
+                  <Container maxWidth="md">
+                    <Grid container direction="row" justify="center"
+                          alignItems="center" spacing={4}>
+                      {isPatient ?
+                          <Grid item xs={12} sm={6} md={4}>
+                            <Card>
+                              <CardContent>
+                                <Typography gutterBottom variant="h5"
+                                            component="h2">
+                                  Game
+                                </Typography>
+                              </CardContent>
+                              <CardActions>
+                                <RouterLink to={`/game`}>
+                                  <Button assize="small" color="primary">
+                                    Start
+                                  </Button>
+                                </RouterLink>
+                              </CardActions>
+                            </Card>
+                          </Grid>
+                          : null}
+
+                      <Grid item xs={12} sm={6} md={4}>
+                        <Card>
+                          <CardContent>
+                            <Typography gutterBottom variant="h5"
+                                        component="h2">
+                              Result
+                            </Typography>
+                          </CardContent>
+                          <CardActions>
+                            <RouterLink to={`/result`}>
+                              <Button assize="small" color="primary">
+                                view
+                              </Button>
+                            </RouterLink>
+                          </CardActions>
+                        </Card>
+                      </Grid>
+                    </Grid>
+                    <Button
+                        data-testid="loginButton"
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        href="/logout"
+                        style={{
+                          marginLeft: 400,
+                          width: 120,
+                          height: 50,
+                          marginTop: 30,
+                          marginBottom: 30
+                        }}
+                    >Logout</Button>
+                  </Container>
+                  <div>
+                    <Switch>
+                      <Route path="/game" component={Game}/>
+                      <Route path="/result" component={ResultController}/>
+                    </Switch>
+                  </div>
+                  {/* <AppRouter/> */}
+                </Router>
+              </main>
+              {/* Footer */}
+              <footer
+              >
+                <Typography variant="subtitle1" align="center"
+                            color="textSecondary" style={{marginTop: 90}}
+                            component="p">
+                  This is a school project for CZ3002.
+                </Typography>
+                <Copyright/>
+              </footer>
+              {/* End footer */}
+
+
+            </React.Fragment>
+
+          </Provider>
+
+      )
+    } else {
 
       return (
-        <loader />
+          <loader/>
       )
     }
-
 
   }
 }
 
-
-
-
 Header.propTypes = {
-
 
   userLoading: PropTypes.bool.isRequired,
   userFailed: PropTypes.bool,
@@ -283,13 +287,11 @@ Header.propTypes = {
   // fetchMe: PropTypes.func.isRequired,
 };
 
-
 const mapStateToProps = (state) => ({
 
   userLoading: selectUserLoading(state),
   userFailed: selectUserFailed(state),
   user: selectUser(state),
-
 
 });
 
@@ -298,6 +300,5 @@ const dispatchers = {
   // fetchMe
 
 };
-
 
 export default connect(mapStateToProps, dispatchers)(Header);
--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
-import { Button, CssBaseline, TextField, Container, Typography } from "@material-ui/core";
+import React, {Component} from "react";
 import {
-  fetchMe,
-  selectUser,
-  selectUserLoading,
-  selectUserFailed,
-  authenticateLogin
-} from "../../redux/ducks/auth";
-import { Redirect } from "react-router-dom";
+  Button,
+  Container,
+  CssBaseline,
+  TextField,
+  Typography
+} from "@material-ui/core";
+import {authenticateLogin, fetchMe, selectUser} from "../../redux/ducks/auth";
 import PropTypes from "prop-types";
-import { connect } from "react-redux";
+import {connect} from "react-redux";
 
 class LoginPage extends Component {
   constructor(props) {
@@ -18,134 +17,120 @@ class LoginPage extends Component {
     this.state = {
       username: "",
       password: "",
-      role:""
     };
 
   }
+
   handleChange = (e) => {
     this.setState({
       [e.target.name]: e.target.value
     });
   };
 
-  // calcRedirect = (e) => {
-  //   console.log(JSON.stringify(e));
-  //   if (e.role === "ROLE_ADMIN") {
-  //     this.props.history.push('/systemadmin');
-  //   } else {
-  //      this.props.history.push('/main');
-  //   }
-  // }
-
   handleSubmit = (e) => {
     e.preventDefault();
-    try {
-      this.setState({ username: "", password: "" });
-      const { username, password } = this.state;
-      this.props.login(this.state);     
-      this.props.history.push('/main');
-    } catch (e) {
-      window.alert('Invalid Credentials!')
-      alert(e.message);
-    }
+    this.props.authenticateLogin(this.state);
+    // try {
+    //   this.setState({username: "", password: ""});
+    //   const {username, password} = this.state;
+    //   this.props.authenticateLogin(this.state);
+    //   this.props.history.push('/main');
+    // } catch (e) {
+    //   window.alert('Invalid Credentials!')
+    //   alert(e.message);
+    // }
   };
 
   render() {
 
     return (
-      <div className="main" data-test="loginContainer">
-        <Container component="main" maxWidth="xs" align="center" style={{ marginTop: 150 }} >
-          <CssBaseline />
-          <div className="login-form">
-            <Typography variant="h3" style={{ marginBottom: 20 }}>Login</Typography>
+        <div className="main" data-test="loginContainer">
+          <Container component="main" maxWidth="xs" align="center"
+                     style={{marginTop: 150}}>
+            <CssBaseline/>
+            <div className="login-form">
+              <Typography variant="h3"
+                          style={{marginBottom: 20}}>Login</Typography>
 
-            <form
-              noValidate
-              onSubmit={this.handleSubmit}>
-              {this.props.fetchMeSuccessAction ? "Logged in" : ""}
-              {this.props.fetchMeRequestAction && !this.props.fetchMeSuccessAction ? "Logging.." : ""}
-              <TextField
-                data-testid="usernameField"
-                onChange={this.handleChange}
-                value={this.state.username}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                id="username"
-                label="Username"
-                name="username"
-                autoComplete="username"
-                autoFocus
-              //style={{height:25, width:200, marginLeft:55}}
-              />
+              <form
+                  noValidate
+                  onSubmit={this.handleSubmit}>
+                <TextField
+                    data-testid="usernameField"
+                    onChange={this.handleChange}
+                    value={this.state.username}
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    id="username"
+                    label="Username"
+                    name="username"
+                    autoComplete="username"
+                    autoFocus
+                />
 
-              <TextField
-                data-testid="passwordField"
-                onChange={this.handleChange}
-                value={this.state.password}
-                variant="outlined"
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              //style={{height:25, width:200, marginLeft:55}}
-              />
-              <div className="form-group">
-                <Button
-                  data-testid="loginButton"
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  color="secondary"
-                  style={{ marginTop: 20 }}
-                >Login</Button>
-              </div>
-            </form>
-          </div>
-        </Container>
-      </div>
+                <TextField
+                    data-testid="passwordField"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                    variant="outlined"
+                    margin="normal"
+                    required
+                    fullWidth
+                    name="password"
+                    label="Password"
+                    type="password"
+                    id="password"
+                    autoComplete="current-password"
+                />
+                <div className="form-group">
+                  <Button
+                      data-testid="loginButton"
+                      type="submit"
+                      fullWidth
+                      variant="contained"
+                      color="secondary"
+                      style={{marginTop: 20}}
+                  >Login</Button>
+                </div>
+              </form>
+            </div>
+          </Container>
+        </div>
     );
   }
 
 }
+
 LoginPage.propTypes = {
   /** An action creator for authenticating login */
   authenticateLogin: PropTypes.func.isRequired,
-  fetchMeSuccessAction: PropTypes.func.isRequired,
-  fetchMeRequestAction: PropTypes.func.isRequired,
-  userFailed: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  fetchMe: PropTypes.func.isRequired
+  // fetchMeSuccessAction: PropTypes.func.isRequired,
+  // fetchMeRequestAction: PropTypes.func.isRequired,
+  // userFailed: PropTypes.bool,
+  // user: PropTypes.object.isRequired,
+  // fetchMe: PropTypes.func.isRequired
   /** An object used for styling */
 };
 
 const dispatch = {
-  authenticateLogin,
-  fetchMe
+  authenticateLogin
 };
 
+// const mapStateToProps = state => {
+//   return {
+//     fetchMeSuccessAction: state.fetchMeSuccessAction,
+//     fetchMeRequestAction: state.fetchMeRequestAction,
+//
+//     user: selectUser(state)
+//   };
+// };
 
+// const mapDispatchToProps = dispatch => {
+//   return {
+//     login: data => dispatch(authenticateLogin(data))
+//   };
+// };
 
-const mapStateToProps = state => {
-  return {
-    fetchMeSuccessAction: state.fetchMeSuccessAction,
-    fetchMeRequestAction: state.fetchMeRequestAction,
-
-    user: selectUser(state)
-  };
-};
-
-const mapDispatchToProps = dispatch => {
-  return {
-    login: data => dispatch(authenticateLogin(data))
-  };
-};
-// const connectedLoginPage = connect(mapStateToProps)(LoginPage);
-// export { connectedLoginPage as LoginPage }; 
-export default connect(mapStateToProps,
-  mapDispatchToProps)(LoginPage);
+export default connect(mapStateToProps, mapDispatchToProps)(LoginPage);
