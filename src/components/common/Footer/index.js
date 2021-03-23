cc06ec7f52d84e0d008a66802539850dfa6298d5
suvoonhou <voonhousu@gmail.com>
2021-03-23 15:35:51 +0800

    Footer fix 3

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {makeStyles} from '@material-ui/core/styles';
 import Link from '@material-ui/core/Link';
 
 function Copyright() {
@@ -19,20 +17,34 @@ function Copyright() {
 }
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  icon: {
+    marginRight: theme.spacing(2),
+  },
+  heroContent: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(8, 0, 6),
+  },
+  heroButtons: {
+    marginTop: theme.spacing(4),
+  },
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
   },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
+  cardMedia: {
+    paddingTop: '56.25%', // 16:9
+  },
+  cardContent: {
+    flexGrow: 1,
   },
   footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-        theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
   },
 }));
 
@@ -40,13 +52,15 @@ export default function Footer() {
   const classes = useStyles();
 
   return (
-      <div className={classes.root}>
-        <footer className={classes.footer}>
-          <Container maxWidth="sm">
-            <Typography variant="body1">My sticky footer can be found here.</Typography>
-            <Copyright />
-          </Container>
-        </footer>
-      </div>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+          Footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary"
+                    component="p">
+          Something here to give the footer a purpose!
+        </Typography>
+        <Copyright/>
+      </footer>
   );
 }
