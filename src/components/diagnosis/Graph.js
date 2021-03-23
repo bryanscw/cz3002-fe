d76a4ad9049bed82a46f1e3be872c59c8d81cf2d
yeruiyi <yeruiyi0@gmail.com>
2021-03-23 11:02:26 +0800

    Graph

--- src/components/diagnosis/Graph.js
+++ src/components/diagnosis/Graph.js
@@ -1,30 +1,94 @@
 
 import React, { Component } from "react";
-import { Line } from "react-chartjs-2";
+import { Bar } from "react-chartjs-2";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 
-const data = {
-  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
-  datasets: [
-    {
-      label: "First dataset",
-      data: [33, 53, 85, 41, 44, 65],
-      fill: true,
-      lineTension: 0,
-      backgroundColor: "rgba(75,192,192,0.2)",
-      borderColor: "rgba(75,192,192,1)"
-    },
-    
-  ]
-};
+import {
+  fetchAccuracy,
+  fetchTime,
+  selectGraphLoading,
+  selectGraphFailed,
+  selectGraph
+
+} from '../redux/ducks/graph.js'
+
 class Graph extends Component {
+  constructor(props) {
+    super(props);
+   
+    this.state = {
 
+  
+      number_of_bins : 10
+ 
+      
+    };
+  }
+  componentDidMount(){
+   
+    this.props.fetchAccuracy(this.state)
+    this.props.fetchTime(this.state)
+     //this.props.fetchMe(this.state)
+    
+   }
     render=()=>{
-
+      if(!this.props.graphLoading && !this.props.graphFailed ){
+        console.log(this.props.graph)
+     
+     
+      const data = {
+        labels: this.props.graph.labels,
+        datasets: [
+          {
+            label: "dataset",
+            data: this.props.graph.data,
+            fill: true,
+            lineTension: 0,
+            backgroundColor: "#115293",
+            borderColor: "rgba(75,192,192,1)"
+          },
+          
+        ]
+      };
         return (
             <div className="App">
-              <Line data={data} />
+              <Bar data={data} />
             </div>
           );
+        }
+        else{
+          return(
+            <loader></loader>
+          )
+        }
     }
 }
-export default Graph;
+Graph.propTypes = {
+ 
+  fetchAccuracy: PropTypes.func.isRequired,
+  fetchTime: PropTypes.func.isRequired,
+  graphLoading:PropTypes.bool.isRequired,
+  graphFailed:PropTypes.bool,
+  graph:PropTypes.object.isRequired,
+
+
+};
+
+
+const mapStateToProps = (state) =>({
+  graphLoading:selectGraphLoading(state),
+  graphFailed:selectGraphFailed(state),
+  graph:selectGraph(state),
+
+
+
+});
+
+const dispatchers = {
+  fetchAccuracy,
+  fetchTime,
+
+};
+
+export default connect(mapStateToProps, dispatchers)(Graph);
