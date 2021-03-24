a8b2bac9758d234fab06bb5255a557d39a638fcb
suvoonhou <voonhousu@gmail.com>
2021-03-24 14:02:33 +0800

    Fix homepage 5

--- src/components/common/HomePage/index.js
+++ src/components/common/HomePage/index.js
@@ -1,24 +1,48 @@
-import React from 'react'
+import React, {Component} from 'react';
 import {Container, Typography} from "@material-ui/core"
+import {selectUser} from "../../../redux/ducks/auth";
+import Button from '@material-ui/core/Button';
 
-/**
- * This component displays a page when requested content is not found.
- */
-export default function HomePage() {
-  return (
-      <div>
-        <Container maxWidth="sm">
-          <Typography component="h1" variant="h2" align="center"
-                      color="textPrimary" gutterBottom>
-            Trail Making Test
-          </Typography>
-          <Typography variant="h5" align="center"
-                      color="textSecondary" paragraph>
-            Trail Making Test is a simple neuropsychological test of
-            cognitive processes, including attention, visual search
-            and scanning, and psychomotor speed.
-          </Typography>
-        </Container>
-      </div>
-  );
+export class HomePage extends Component {
+  render() {
+    const {
+      user
+    } = this.props;
+
+    return (
+        <div>
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center"
+                        color="textPrimary" gutterBottom>
+              Trail Making Test
+            </Typography>
+            <Typography variant="h5" align="center"
+                        color="textSecondary" paragraph>
+              Trail Making Test is a simple neuropsychological test of
+              cognitive processes, including attention, visual search
+              and scanning, and psychomotor speed.
+            </Typography>
+          </Container>
+          {
+            user && Object.keys(user).length !== 0 ? (
+                <Button variant="contained" color="primary" href="/">
+                  To Dashboard
+                </Button>
+            ) : ({})
+          }
+        </div>
+    );
+  }
+
 }
+
+const mapStateToProps = state => ({
+      user: selectUser
+      (
+          state
+      ),
+    }
+
+);
+
+export default connect(mapStateToProps)(HomePage);
