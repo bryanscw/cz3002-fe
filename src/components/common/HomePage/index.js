97c43aacf0163aa50d7684d4cdb9402b84c7f4bf
suvoonhou <voonhousu@gmail.com>
2021-03-24 13:51:02 +0800

    Fix homepage

--- src/components/common/HomePage/index.js
+++ src/components/common/HomePage/index.js
@@ -1,11 +1,23 @@
 import React from 'react'
-import {Header} from "../Header";
 
 /**
  * This component displays a page when requested content is not found.
  */
 export default function HomePage() {
   return (
-      <Header/>
-  )
+      <div>
+        <Container maxWidth="sm">
+          <Typography component="h1" variant="h2" align="center"
+                      color="textPrimary" gutterBottom>
+            Trail Making Test
+          </Typography>
+          <Typography variant="h5" align="center"
+                      color="textSecondary" paragraph>
+            Trail Making Test is a simple neuropsychological test of
+            cognitive processes, including attention, visual search
+            and scanning, and psychomotor speed.
+          </Typography>
+        </Container>
+      </div>
+  );
 }
