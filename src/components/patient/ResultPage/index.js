10af0f7ede7f192a2c7c8ccdf67c4236b965a2ee
suvoonhou <voonhousu@gmail.com>
2021-03-25 02:59:48 +0800

    a1

--- src/components/patient/ResultPage/index.js
+++ src/components/patient/ResultPage/index.js
@@ -19,12 +19,6 @@ class ResultPage extends Component {
     this.props.listUserResults(this.state);
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.match.params.resultId !== nextProps.match.params.resultId) {
-      this.resultId = parseInt(nextProps.match.params.resultId);
-    }
-  }
-
   render() {
     const {
       resultsLoading,
