89d5aa0676c571a9757ae6b2e7743bfefeaf4ad1
suvoonhou <voonhousu@gmail.com>
2021-03-24 13:53:51 +0800

    Fix homepage 3

--- src/components/common/AppRouter/index.js
+++ src/components/common/AppRouter/index.js
@@ -49,8 +49,8 @@ class AppRouter extends Component {
 
     let routes = [
       <Route key="Login" path="/login" exact component={LoginPage}/>,
-      <Route key="Home" path="/home" exact component={HomePage}/>,
       <Redirect key="LoginRedirect" from="/" exact to="/login"/>,
+      <Route key="Home" path="/home" exact component={HomePage}/>,
     ];
 
     // Check if a valid user is logged in
