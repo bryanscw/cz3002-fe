842e8481b157f1c7fbf0328f05d70fcd63c7459e
suvoonhou <voonhousu@gmail.com>
2021-03-24 10:09:24 +0800

    Fixed routing

--- src/components/common/ResultCard/index.js
+++ src/components/common/ResultCard/index.js
@@ -15,11 +15,12 @@ export default function ResultCard(props) {
       <div className={`card${classes ? ` ${classes}` : ""}`}>
         <div className="card-body">
           <h3 className="card-title">
-            <Link to={link}>{result.id}</Link> {badge}
+            Test id: {result.id} {badge}
           </h3>
           <p className="card-text">{result.numNodes}</p>
           <p className="card-text">{result.accuracy}</p>
           <p className="card-text">{result.time}</p>
+          <Link to={link}>More details...</Link>
         </div>
       </div>
   )
