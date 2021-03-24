9090b09f8ffe42c4e110a7ee46d705850bd3ecd4
suvoonhou <voonhousu@gmail.com>
2021-03-24 21:35:07 +0800

    Fix Header

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -2,71 +2,74 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 import {Link} from "react-router-dom";
 import {selectUser} from "../../../redux/ducks/auth";
-import {
-    AppBar,
-    Box,
-    Button,
-    withStyles,
-    Toolbar,
-    Typography
-} from "@material-ui/core";
-import { red, green } from "@material-ui/core/colors"
-import "./index.css";
+import {AppBar, Box, Toolbar, Typography} from "@material-ui/core";
+import {green, red} from "@material-ui/core/colors"
+import {makeStyles} from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button'
+
+const useStyles = makeStyles((theme) => ({
+  header: {
+    "-webkit-text-fill-color": "#cccccc",
+  },
+
+  loginButton: {
+    float: "right",
+    color: theme.palette.getContrastText(green[500]),
+    backgroundColor: green[500],
+    '&:hover': {
+      backgroundColor: green[700],
+    },
+  },
+
+  logoutButton: {
+    float: "right",
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
+    '&:hover': {
+      backgroundColor: red[700],
+    },
+  }
+}));
 
 /**
  * This component displays the header of the web application.
  */
 export class Header extends Component {
   render() {
+    const classes = useStyles();
+
     const {
       user
     } = this.props;
 
-    const LogoutButton = withStyles((theme) => ({
-        root: {
-            color: theme.palette.getContrastText(red[500]),
-            backgroundColor: red[500],
-            '&:hover': {
-                backgroundColor: red[700],
-            },
-        },
-    }))(Button);
-
-    const LoginButton = withStyles((theme) => ({
-        root: {
-            color: theme.palette.getContrastText(green[500]),
-            backgroundColor: green[500],
-            '&:hover': {
-                backgroundColor: green[700],
-            },
-        },
-    }))(Button);
-
     const authLinks = (
         <Box className="logout">
-            <LogoutButton size="small" component={ Link } to="/logout">
-                Logout
-            </LogoutButton>
+          <Button className={classes.logoutButton} size="small" component={Link}
+                  to="/logout">
+            Logout
+          </Button>
         </Box>
     );
 
     const guestLinks = (
-            <LoginButton className="login" size="small" component={ Link } to="/login">
-                Login
-            </LoginButton>
+        <Button className={classes.loginButton} size="small" component={Link}
+                to="/login">
+          Login
+        </Button>
     );
 
     return (
-            <AppBar position="static">
-                <Toolbar>
-                    <Typography style={{flex: 1}} variant="h6" color="inherit" noWrap>
-                        <Link className="header" style={{textDecoration: "none"}} to="/home">
-                                TRAIL MAKING TEST
-                        </Link>
-                    </Typography>
-                    {user && Object.keys(user).length !== 0 ? authLinks : guestLinks}
-                </Toolbar>
-            </AppBar>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography style={{flex: 1}} variant="h6" color="inherit" noWrap>
+              <Link className="header" style={{textDecoration: "none"}}
+                    to="/home">
+                TRAIL MAKING TEST
+              </Link>
+            </Typography>
+            {user && Object.keys(user).length !== 0 ? authLinks : guestLinks}
+          </Toolbar>
+        </AppBar>
     );
   }
 }
