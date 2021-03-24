20435a6076773e9ec1c133fc6dfcd2348d983da0
suvoonhou <voonhousu@gmail.com>
2021-03-24 14:16:27 +0800

    Fix Homepage 3

--- src/components/common/HomePage/index.js
+++ src/components/common/HomePage/index.js
@@ -11,28 +11,30 @@ export class HomePage extends Component {
     } = this.props;
 
     return (
-        <Container maxWidth="sm">
-          <Typography component="h1" variant="h2" align="center"
-                      color="textPrimary" gutterBottom>
-            Trail Making Test
-          </Typography>
-          <Typography variant="h5" align="center"
-                      color="textSecondary" paragraph>
-            Trail Making Test is a simple neuropsychological test of
-            cognitive processes, including attention, visual search
-            and scanning, and psychomotor speed.
-          </Typography>
-          {
-            user && Object.keys(user).length !== 0 ? (
-                <Button variant="contained" color="primary" href="/">
-                  To Dashboard
-                </Button>
-            ) : (
-                <p></p>
-            )
-          }
+        <div className="main">
+          <Container maxWidth="sm">
+            <Typography component="h1" variant="h2" align="center"
+                        color="textPrimary" gutterBottom>
+              Trail Making Test
+            </Typography>
+            <Typography variant="h5" align="center"
+                        color="textSecondary" paragraph>
+              Trail Making Test is a simple neuropsychological test of
+              cognitive processes, including attention, visual search
+              and scanning, and psychomotor speed.
+            </Typography>
+            {
+              user && Object.keys(user).length !== 0 ? (
+                  <Button variant="contained" color="primary" href="/">
+                    To Dashboard
+                  </Button>
+              ) : (
+                  <p></p>
+              )
+            }
 
-        </Container>
+          </Container>
+        </div>
     );
   }
 
