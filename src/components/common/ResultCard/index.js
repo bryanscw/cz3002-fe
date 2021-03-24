3cc6e2372a16202c38accba5e5292a71fe6ba19f
suvoonhou <voonhousu@gmail.com>
2021-03-24 10:14:35 +0800

    ResultCard fix 5

--- src/components/common/ResultCard/index.js
+++ src/components/common/ResultCard/index.js
@@ -20,7 +20,7 @@ export default function ResultCard(props) {
           <p className="card-text">Accuracy: {result.accuracy}%</p>
           <p className="card-text">Time taken: {result.time} seconds</p>
           <Link to={`/results/${result.id}`}>More details...</Link>
-          <Link to={`/diagnosis/`${result.id}}>Diagnosis</Link>
+          <Link to={`/diagnosis/${result.id}`}>Diagnosis</Link>
         </div>
       </div>
   )
