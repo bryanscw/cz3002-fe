675e671b019a30143f736640f961a595025df4a3
bryanscw <csin001@e.ntu.edu.sg>
2021-03-30 12:02:18 +0800

    Updated patient result view

--- src/components/patient/PatientHomePage/index.js
+++ src/components/patient/PatientHomePage/index.js
@@ -1,137 +1,129 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
-  listUserResults,
-  selectResults,
-  selectResultsFailed,
-  selectResultsLoading,
+    listUserResults,
+    selectResults,
+    selectResultsFailed,
+    selectResultsLoading,
 } from '../../../redux/ducks/result';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
-import { Redirect } from 'react-router-dom';
-import { selectUser } from '../../../redux/ducks/auth';
-import { CircularProgress, Typography } from '@material-ui/core';
-import { Alert, AlertTitle } from '@material-ui/lab';
+import {Redirect} from 'react-router-dom';
+import {selectUser} from '../../../redux/ducks/auth';
+import {CircularProgress} from '@material-ui/core';
+import {Alert, AlertTitle} from '@material-ui/lab';
 import ResultCard from '../ResultCard';
 import PendingTestCard from '../PendingTestCard';
 
 class PatientHomePage extends Component {
-  componentDidMount() {
-    this.props.listUserResults(this.state);
-  }
-
-  render() {
-    const {
-      resultsLoading,
-      resultsFailed,
-      results,
-      user,
-    } = this.props;
-
-    if (resultsLoading) {
-      return <CircularProgress align="center"
-        style={{
-          marginTop: 200,
-          marginLeft: 860,
-        }} />;
+    componentDidMount() {
+        this.props.listUserResults(this.state);
     }
 
-    if (resultsFailed) {
-      return <Redirect to="/not-found" />;
+    render() {
+        const {
+            resultsLoading,
+            resultsFailed,
+            results,
+            user,
+        } = this.props;
+
+        if (resultsLoading) {
+            return <CircularProgress align="center"
+                                     style={{
+                                         marginTop: 200,
+                                         marginLeft: 860,
+                                     }}/>;
+        }
+
+        if (resultsFailed) {
+            return <Redirect to="/not-found"/>;
+        }
+
+        // Find tests that have been completed
+        let completedTests = results.filter(
+            function (el) {
+                // Completed tests will have a non-null time or accuracy
+                return el.time || el.accuracy;
+            },
+        );
+
+        // Find tests that have not been completed
+        let pendingTests = results.filter(
+            function (el) {
+                // Completed tests will have a non-null time or accuracy
+                return !el.time && !el.accuracy;
+            },
+        );
+
+        return (
+            <div className="container">
+
+                {
+                    pendingTests.length !== 0 ? (
+                            pendingTests.map(
+                                result =>
+                                    <PendingTestCard
+                                        key={result.id}
+                                        classes="mb-4"
+                                        result={result}
+                                    />,
+                            )
+                        )
+                        :
+                        (
+                            <Alert severity="info">
+                                <AlertTitle>Info</AlertTitle>
+                                <strong>No</strong> pending test(s) found
+                            </Alert>
+                        )
+                }
+
+                {
+                    completedTests.length !== 0 ? (
+                            <ResultCard
+                                key={completedTests.id}
+                                classes="mb-4"
+                                result={completedTests}
+                                badge={completedTests.accuracy && completedTests.time ? <span
+                                        className="badge badge-success">Completed</span>
+                                    : <span className="badge badge-secondary">Not Completed</span>}
+                            />
+                        )
+                        :
+                        (
+                            <Alert severity="info">
+                                <AlertTitle>Info</AlertTitle>
+                                <strong>No</strong> result(s) found
+                            </Alert>
+                        )
+                }
+
+            </div>
+        );
     }
-
-    // Find tests that have been completed
-    let completedTests = results.filter(
-      function (el) {
-        // Completed tests will have a non-null time or accuracy
-        return el.time || el.accuracy;
-      },
-    );
-
-    // Find tests that have not been completed
-    let pendingTests = results.filter(
-      function (el) {
-        // Completed tests will have a non-null time or accuracy
-        return !el.time && !el.accuracy;
-      },
-    );
-
-    return (
-      <div className="container">
-        <Typography variant="h1">Results for: {user.name}</Typography>
-
-        <Typography variant="h2">Pending Test(s)</Typography>
-        {
-          pendingTests.length !== 0 ? (
-              pendingTests.map(
-                result =>
-                  <PendingTestCard
-                    key={result.id}
-                    classes="mb-4"
-                    result={result}
-                  />,
-              )
-            )
-            :
-            (
-              <Alert severity="info">
-                <AlertTitle>Info</AlertTitle>
-                <strong>No</strong> pending test(s) found
-              </Alert>
-            )
-        }
-
-        <Typography variant="h2">Completed Test(s)</Typography>
-        <br />
-
-        {
-          completedTests.length !== 0 ? (
-              completedTests.map(
-                result =>
-                  <ResultCard
-                    key={result.id}
-                    classes="mb-4"
-                    result={result}
-                    badge={result.accuracy && result.time ? <span
-                        className="badge badge-success">Completed</span>
-                      : <span className="badge badge-secondary">Not Completed</span>}
-                  />,
-              )
-            )
-            :
-            (
-              <Alert severity="info">
-                <AlertTitle>Info</AlertTitle>
-                <strong>No</strong> result(s) found
-              </Alert>
-            )
-        }
-
-      </div>
-    );
-  }
 }
 
 PatientHomePage.propTypes = {
-  /** An action creator */
-  listUserResults: PropTypes.func.isRequired,
-  /** A boolean to determine if the results are still being loaded (true: still loading, false: fully loaded) */
-  resultsLoading: PropTypes.bool.isRequired,
-  /** A boolean to determine if the users failed to be loaded the action creator(true: still loading or failed to load, false: successful load) */
-  resultsFailed: PropTypes.bool,
-  /** An array of results objects loaded by the action creator */
-  results: PropTypes.array.isRequired,
-  user: PropTypes.object,
+    /** An action creator */
+    listUserResults: PropTypes.func.isRequired,
+    /** A boolean to determine if the results are still being loaded (true: still loading, false: fully loaded) */
+    resultsLoading: PropTypes.bool.isRequired,
+    /** A boolean to determine if the users failed to be loaded the action creator(true: still loading or failed to load, false: successful load) */
+    resultsFailed: PropTypes.bool,
+    /** An array of results objects loaded by the action creator */
+    results: PropTypes.array.isRequired,
+    user: PropTypes.object,
 };
 
 const mapStateToProps = state => ({
-  resultsLoading: selectResultsLoading(state),
-  resultsFailed: selectResultsFailed(state),
-  results: selectResults(state),
-  user: selectUser(state),
+    resultsLoading: selectResultsLoading(state),
+    resultsFailed: selectResultsFailed(state),
+    results: selectResults(state),
+    user: selectUser(state),
 });
 
 const dispatchers = {
-  listUserResults,
+    listUserResults,
 };
 
 export default connect(mapStateToProps, dispatchers)(PatientHomePage);
--- src/components/patient/PendingTestCard/index.js
+++ src/components/patient/PendingTestCard/index.js
@@ -1,31 +1,75 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
 
 /**This component is used to display an item with a title and description */
 export default function PendingTestCard(props) {
-  const {
-    classes,
-    result,
-  } = props;
+    const {
+        classes,
+        result,
+    } = props;
 
-  return (
-    <div className={`card${classes ? ` ${classes}` : ''}`}>
-      <div className="card-body">
-        <h3 className="card-title">
-          Test id: {result.id}
-        </h3>
-        <p className="card-text">Number of nodes: {result.nodeNum}</p>
-        <br />
-        <Button color="primary" href={`/game/${result.id}`}>Do Test</Button>
-      </div>
-    </div>
-  );
+    return (
+        <div className={`card${classes ? ` ${classes}` : ''}`}
+             style={{
+                 width: '100%',
+                 padding: '40px',
+             }}>
+            <MaterialTable
+                title='Pending tests'
+                font=""
+                columns={[
+                    {
+                        title: 'ID',
+                        field: 'id'
+                    },
+                    {
+                        title: "Accuracy",
+                        field: "accuracy"
+                    },
+                    {
+                        title: "Time",
+                        field: "time"
+                    },
+                    {
+                        title: 'Number of Nodes',
+                        field: 'nodeNum',
+                    },
+                    {
+                        title: 'Result',
+                        render: (result) => (
+                            <Button
+                                color="primary"
+                                href={`/game/${result.id}`}
+                                variant='outlined'
+                            >Do Test
+                            </Button>
+                        ),
+                    },
+                ]}
+                data={[result]}
+                options={{
+                    search: false,
+                    paging: false,
+                    cellStyle: {
+                        fontFamily: 'Helvetica',
+                        fontSize: 15,
+                    },
+                    headerStyle: {
+                        backgroundColor: '#d32f2f',
+                        color: '#FFF',
+                        fontSize: 17,
+                    },
+                }}
+            />
+        </div>
+    );
 }
 
 PendingTestCard.propTypes = {
-  /** A string storing the html to be included in the component */
-  classes: PropTypes.string,
-  /** An object containing the item's title and description */
-  result: PropTypes.object.isRequired,
+    /** A string storing the html to be included in the component */
+    classes: PropTypes.string,
+    /** An object containing the item's title and description */
+    result: PropTypes.object.isRequired,
 };
