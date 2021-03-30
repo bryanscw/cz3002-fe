942c80f0892f339191d9098e5bffb87ffde95e01
bryanscw <38095231+bryanscw@users.noreply.github.com>
2021-03-30 12:44:36 +0800

    Updated patient result view (#69)
    * Updated patient result view
    
    * Removed unused component
    
    * Updated README.md for deployment instructions
    
    * Updated code style

--- src/components/patient/PendingTestCard/index.js
+++ src/components/patient/PendingTestCard/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
 
 /**This component is used to display an item with a title and description */
 export default function PendingTestCard(props) {
@@ -10,16 +11,59 @@ export default function PendingTestCard(props) {
   } = props;
 
   return (
-    <div className={`card${classes ? ` ${classes}` : ''}`}>
-      <div className="card-body">
-        <h3 className="card-title">
-          Test id: {result.id}
-        </h3>
-        <p className="card-text">Number of nodes: {result.nodeNum}</p>
-        <br />
-        <Button color="primary" href={`/game/${result.id}`}>Do Test</Button>
+      <div className={`card${classes ? ` ${classes}` : ''}`}
+           style={{
+             width: '100%',
+             padding: '40px',
+           }}>
+        <MaterialTable
+            title='Pending tests'
+            font=""
+            columns={[
+              {
+                title: 'ID',
+                field: 'id',
+              },
+              {
+                title: 'Accuracy',
+                field: 'accuracy',
+              },
+              {
+                title: 'Time',
+                field: 'time',
+              },
+              {
+                title: 'Number of Nodes',
+                field: 'nodeNum',
+              },
+              {
+                title: 'Result',
+                render: (result) => (
+                    <Button
+                        color="primary"
+                        href={`/game/${result.id}`}
+                        variant='outlined'
+                    >Do Test
+                    </Button>
+                ),
+              },
+            ]}
+            data={[result]}
+            options={{
+              search: false,
+              paging: false,
+              cellStyle: {
+                fontFamily: 'Helvetica',
+                fontSize: 15,
+              },
+              headerStyle: {
+                backgroundColor: '#d32f2f',
+                color: '#FFF',
+                fontSize: 17,
+              },
+            }}
+        />
       </div>
-    </div>
   );
 }
 
--- src/components/patient/ResultCard/index.js
+++ src/components/patient/ResultCard/index.js
@@ -1,27 +1,71 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import MaterialTable from 'material-table';
 
 /**This component is used to display an item with a title and description */
 export default function ResultCard(props) {
   const {
     classes,
     result,
-    badge,
   } = props;
 
   return (
-    <div className={`card${classes ? ` ${classes}` : ''}`}>
-      <div className="card-body">
-        <h3 className="card-title">
-          Test id: {result.id} - {badge}
-        </h3>
-        <p className="card-text">Number of nodes: {result.nodeNum}</p>
-        <p className="card-text">Accuracy: {result.accuracy}%</p>
-        <p className="card-text">Time taken: {result.time} seconds</p>
-        <Button color="primary" href={`/result/${result.id}`}>More details...</Button>
+      <div className={`card${classes ? ` ${classes}` : ''}`}
+           style={{
+             width: '100%',
+             padding: '40px',
+           }}>
+        <MaterialTable
+            title='Completed Tests'
+            font=""
+            columns={[
+              {
+                title: 'ID',
+                field: 'id',
+              },
+              {
+                title: 'Accuracy',
+                field: 'accuracy',
+              },
+              {
+                title: 'Time',
+                field: 'time',
+              },
+              {
+                title: 'Number of Nodes',
+                field: 'nodeNum',
+              },
+              {
+                title: 'Diagnosis',
+                render: (result) => (
+                    <Button
+                        color="primary"
+                        href={`/result/${result.id}`}
+                        variant='outlined'
+                    >More details...
+                    </Button>
+                ),
+              },
+            ]}
+            data={result}
+            options={{
+              paginationType: 'stepped',
+              showFirstLastPageButtons: false,
+              paging: false,
+              search: false,
+              cellStyle: {
+                fontFamily: 'Helvetica',
+                fontSize: 15,
+              },
+              headerStyle: {
+                backgroundColor: '#388e3c',
+                color: '#FFF',
+                fontSize: 17,
+              },
+            }}
+        />
       </div>
-    </div>
   );
 }
 
@@ -32,5 +76,5 @@ ResultCard.propTypes = {
   result: PropTypes.object.isRequired,
   /** A badge for the component */
   badge: PropTypes.oneOfType(
-    [PropTypes.string, PropTypes.func, PropTypes.element]),
+      [PropTypes.string, PropTypes.func, PropTypes.element]),
 };
